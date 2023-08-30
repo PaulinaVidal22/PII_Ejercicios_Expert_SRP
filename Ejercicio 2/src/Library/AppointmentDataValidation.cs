a332aee807c8bfc1270de5c56c540ff0af2c05b1
PauloinaVidal22 <paulina.vidal@correo.ucu.edu.uy>
2023-08-30 12:11:07 -0300

    .

---
+++ Ejercicio 2/src/Library/AppointmentDataValidation.cs
@@ -0,0 +1,42 @@
+using System;
+
+
+namespace AppointmentService
+{
+    public class AppointmentDataValidation
+    {
+        public bool ValidateData(string name, string id, string phoneNumber, string appointmentPlace, string doctorName)
+        {
+            Boolean isValid = true;
+
+            if (string.IsNullOrEmpty(name))
+            {
+                isValid = false;
+            }
+
+            if (string.IsNullOrEmpty(id))
+            {
+                isValid = false;
+            }
+
+            if (string.IsNullOrEmpty(phoneNumber))
+            {
+
+                isValid = false;
+            }
+
+            if (string.IsNullOrEmpty(appointmentPlace))
+            {
+                isValid = false;
+            }
+
+
+            if (string.IsNullOrEmpty(doctorName))
+            {
+                isValid = false;
+            }
+
+            return isValid;
+    }
+    }
+}
